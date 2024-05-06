4c47651e763df04ab0897c34af51442a3c6cf822
Sofia Gomez <sofia.f.gomez@gmail.com>
2024-05-06 15:45:27 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,15 @@
+function contarCaracteres() {
+    const texto = document.getElementById("texto").value;
+    const contador = document.getElementById("contador");
+    const Caracteres = 100;
+    
+    contador.textContent = Caracteres - texto.length;
+}
+
+function cambiarColor1() {
+    document.body.style.backgroundColor="red"}
+    function cambiarColor2() {
+    document.body.style.backgroundColor="blue"}
+    function cambiarColor3() {
+    document.body.style.backgroundColor="green"}
+
